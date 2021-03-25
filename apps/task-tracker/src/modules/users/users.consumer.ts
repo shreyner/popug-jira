92f92163b01fd4d0879a9537759c8cf78cf90784
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-03-25 23:23:32 +0300

    add consume UserCreated UserRoleChanges in task-tracker

--- apps/task-tracker/src/modules/users/users.consumer.ts
+++ apps/task-tracker/src/modules/users/users.consumer.ts
@@ -5,9 +5,14 @@ import { Event } from '../../../../auth/src/modules/message-bus/event.type';
 import { UserRepository } from '../../repositories/user.repository';
 import { User } from '../../entities'; // FIXME:
 
-type EventUserUpdate = Event<
-  'UserUpdated',
+type EventUserCUD = Event<
+  'UserUpdated' | 'UserCreated',
   { publicId: string; email: string; role: UserRole }
+>;
+
+type EventUserRoleUpdate = Event<
+  'UserRoleChanged',
+  { publicId: string; role: UserRole }
 >;
 
 @Injectable()
@@ -17,13 +22,19 @@ export class UsersConsumer {
     private readonly userRepository: UserRepository,
   ) {}
 
-  async handleEvent(event: EventUserUpdate) {
+  async handleEvent(event: EventUserCUD | EventUserRoleUpdate) {
+    console.log('Consume event', event);
     switch (event.eventName) {
+      case 'UserCreated':
+        await this.userRepository.addUser(event.data);
+        break;
+      case 'UserRoleChanged':
       case 'UserUpdated':
         await this.userRepository.updateByPublicId(event.data);
-        console.log('User Update', event.data);
         break;
       default:
+        // TODO: I don't know
+        console.log('Not handled event', event);
         throw new Error('Event not consumed');
     }
   }
