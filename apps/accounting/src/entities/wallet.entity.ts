8eb15a947c7407ab77e09834ebef53516100354c
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-03-28 20:22:48 +0300

    add entities for accounting domain

---
+++ apps/accounting/src/entities/wallet.entity.ts
@@ -0,0 +1,36 @@
+import {
+  BaseEntity,
+  Collection,
+  Entity,
+  OneToMany,
+  OneToOne,
+  PrimaryKey,
+  Property,
+} from '@mikro-orm/core';
+import { User } from './user.entity';
+import { Transaction } from './transaction.entity';
+import { PayoutCyrcle } from './payout-cyrcle.entity';
+
+@Entity({})
+export class Wallet extends BaseEntity<Wallet, 'id'> {
+  @PrimaryKey({ index: true })
+  id: number;
+
+  @Property()
+  balance: number;
+
+  @OneToOne({ index: true, owner: true, entity: () => User })
+  user: User;
+
+  @OneToMany({
+    entity: () => Transaction,
+    mappedBy: (transactio) => transactio.wallet,
+  })
+  transactions = new Collection<Transaction>(this);
+
+  @OneToMany({
+    entity: () => PayoutCyrcle,
+    mappedBy: (payoutCyrcle) => payoutCyrcle.wallet,
+  })
+  payoutCyrcle = new Collection<PayoutCyrcle>(this);
+}
