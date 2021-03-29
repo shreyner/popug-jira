2371e166a2239b3358faf9820e9f1ecc2694aa60
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-03-29 22:20:37 +0300

    add handle event BE CUD with user

--- apps/accounting/src/modules/users/users.controller.ts
+++ apps/accounting/src/modules/users/users.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
+import { Ctx, EventPattern, Payload } from '@nestjs/microservices';
+import { BaseEvent } from '@app/event-schema-registry/types/base-event.type';
+import { NatsStreamingContext } from '@nestjs-plugins/nestjs-nats-streaming-transport';
+import { UsersConsumer } from './users.consumer';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(
+    @Inject(UsersConsumer)
+    private readonly usersConsumer: UsersConsumer,
+  ) {}
+
+  @EventPattern('user')
+  async listenBEUserEvent(
+    @Payload() data: BaseEvent<any, any>,
+    @Ctx() ctx: NatsStreamingContext,
+  ) {
+    await this.usersConsumer.handleEvent(data);
+    ctx.message.ack();
+  }
+
+  @EventPattern('user-stream')
+  async listenCUDUserEvent(
+    @Payload() data: BaseEvent<any, any>,
+    @Ctx() ctx: NatsStreamingContext,
+  ) {
+    await this.usersConsumer.handleEvent(data);
+    ctx.message.ack();
+  }
+}
