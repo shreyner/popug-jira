4d60972d33e8b1d645e31c1901d5ce2b142a5913
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-08-09 23:04:04 +0300

    Reorganization modules in session module

--- apps/sessions/src/main.ts
+++ apps/sessions/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { SessionsModule } from './sessions.module';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(SessionsModule);
+  const app = await NestFactory.create(AppModule);
+
   await app.listen(3000);
 }
+
 bootstrap();
